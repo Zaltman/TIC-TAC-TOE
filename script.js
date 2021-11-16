079bae968ed98a028fe6c37f19a522aa0b3ea8ff
Zaltman <mariusdruskinis@gmail.com>
2021-11-16 17:33:07 +0200

    Add 'game finished' logic

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let Gameboard = {
       let element = document.createElement('button');
       element.classList.add('square');
       element.setAttribute('data-key', i);
-      element.addEventListener('click', Gameboard.playerPicks);
+      element.addEventListener('click', Gameboard.playerPicks, Game.checkWin);
       document
         .querySelector('.boardContainer')
         .appendChild(element).textContent = this.board[i];
@@ -14,22 +14,23 @@ let Gameboard = {
   },
 
   playerPicks: function (e) {
-    console.log(Gameboard.playerTurn + ' cia player turn');
-    console.log(Gameboard.board[e.target.dataset.key]);
-    if (
-      Gameboard.board[e.target.dataset.key] == ' ' &&
-      Gameboard.playerTurn == 1
-    ) {
-      Gameboard.board[e.target.dataset.key] = 'x';
-      e.target.textContent = 'x';
-      Gameboard.playerTurn++;
-    } else if (
-      Gameboard.board[e.target.dataset.key] == ' ' &&
-      Gameboard.playerTurn == 2
-    ) {
-      Gameboard.board[e.target.dataset.key] = 'o';
-      e.target.textContent = 'o';
-      Gameboard.playerTurn--;
+    if (Game.state == 1) {
+      if (
+        Gameboard.board[e.target.dataset.key] == ' ' &&
+        Gameboard.playerTurn == 1
+      ) {
+        Gameboard.board[e.target.dataset.key] = 'x';
+        e.target.textContent = 'x';
+        Gameboard.playerTurn++;
+      } else if (
+        Gameboard.board[e.target.dataset.key] == ' ' &&
+        Gameboard.playerTurn == 2
+      ) {
+        Gameboard.board[e.target.dataset.key] = 'o';
+        e.target.textContent = 'o';
+        Gameboard.playerTurn--;
+      }
+      if (Game.checkWin() !== undefined);
     }
   },
 };
@@ -51,24 +52,89 @@ let Players = {
   },
 };
 
-Gameboard.drawBoard();
+let Game = {
+  state: 1,
+  checkWin: function checkWin() {
+    // row check
+    let winCond = 0;
+    let i = 0;
+    let lastElementIndex = 3;
+    for (i; i < lastElementIndex; i++) {
+      if (Gameboard.board[i] == 'x') {
+        winCond++;
+      } else if (Gameboard.board[i] == 'o') {
+        winCond--;
+      }
+      if (winCond == 3 || winCond == -3) {
+        Game.state = 0;
+        return 'Player has won';
+      }
+      if (i == 2 || i == 5) {
+        lastElementIndex += 3;
+        winCond = 0;
+      }
+    }
 
-function checkWin() {
-  let winCond = 0;
-  let i = 0;
-  let lastElementIndex = 3;
-  for (i; i < lastElementIndex; i++) {
-    if (Gameboard.board[i] == 'x') {
-      winCond++;
-    } else if (Gameboard.board[i] == 'o') {
-      winCond--;
+    //column check
+    lastElementIndex = 7;
+    i = 0;
+    winCond = 0;
+    for (i; i < lastElementIndex; i += 3) {
+      if (Gameboard.board[i] == 'x') {
+        winCond++;
+      } else if (Gameboard.board[i] == 'o') {
+        winCond--;
+      }
+      if (winCond == 3 || winCond == -3) {
+        Game.state = 0;
+        return 'Player has won';
+      }
+      if (i == 6) {
+        lastElementIndex += 1;
+        winCond = 0;
+        i = -2;
+      }
+      if (i == 7) {
+        lastElementIndex += 1;
+        winCond = 0;
+        i = -1;
+      }
     }
-    if (winCond == 3 || winCond == -3) {
-      return 'Player has won';
+
+    //diagonal check
+    if (
+      Gameboard.board[0] == Gameboard.board[4] &&
+      Gameboard.board[0] == Gameboard.board[8] &&
+      Gameboard.board[0] == 'x'
+    ) {
+      Game.state = 0;
+      return Players.player1 + ' has won';
     }
-    if (i == 2 || i == 5) {
-      lastElementIndex += 3;
-      winCond = 0;
+    if (
+      Gameboard.board[0] == Gameboard.board[4] &&
+      Gameboard.board[0] == Gameboard.board[8] &&
+      Gameboard.board[0] == 'o'
+    ) {
+      Game.state = 0;
+      return Players.player2 + ' has won';
     }
-  }
-}
+
+    if (
+      Gameboard.board[2] == Gameboard.board[4] &&
+      Gameboard.board[2] == Gameboard.board[6] &&
+      Gameboard.board[2] == 'x'
+    ) {
+      Game.state = 0;
+      return Players.player1 + ' has won';
+    }
+    if (
+      Gameboard.board[2] == Gameboard.board[4] &&
+      Gameboard.board[2] == Gameboard.board[6] &&
+      Gameboard.board[2] == 'o'
+    ) {
+      Game.state = 0;
+      return Players.player2 + ' has won';
+    }
+  },
+};
+Gameboard.drawBoard();
